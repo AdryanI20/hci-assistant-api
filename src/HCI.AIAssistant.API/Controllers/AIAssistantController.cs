bfa412ac31dc7889bfff4137813e87d24189190f
AdryanI20 <gameder14@gmail.com>
2025-11-28 10:41:06 +0200

    newlab

--- src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
+++ src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
@@ -3,52 +3,70 @@ using Microsoft.AspNetCore.Mvc;
 using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
 using HCI.AIAssistant.API.Services;
 using HCI.AIAssistant.API.Models.DTOs;
+using Newtonsoft.Json;
+using Microsoft.Azure.Devices;
+using System.Text;
 
-namespace HCI.AIAssistant.API.Controllers
+namespace HCI.AIAssistant.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AIAssistantController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class AIAssistantController : ControllerBase
+    private readonly ISecretsService _secretsService;
+    private readonly IAppConfigurationsService _appConfigurationsService;
+    private readonly IAIAssistantService _aIAssistantService;
+    private readonly IParametricFunctions _parametricFunctions;
+
+    public AIAssistantController(
+        ISecretsService secretsService,
+        IAppConfigurationsService appConfigurationsService,
+        IAIAssistantService aIAssistantService,
+        IParametricFunctions parametricFunctions
+    )
     {
-        private readonly IAIAssistantService _aIAssistantService;
-        private readonly IParametricFunctions _parametricFunctions;
+        _secretsService = secretsService;
+        _appConfigurationsService = appConfigurationsService;
+        _aIAssistantService = aIAssistantService;
+        _parametricFunctions = parametricFunctions;
+    }
 
-        public AIAssistantController(
-            IAIAssistantService aIAssistantService,
-            IParametricFunctions parametricFunctions
-        )
+    [HttpPost("message")]
+    [ProducesResponseType(typeof(AIAssistantControllerPostMessageResponseDTO), 200)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 400)]
+    public async Task<ActionResult> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
+    {
+        if (!_parametricFunctions.ObjectExistsAndHasNoNullPublicProperties(request))
         {
-            _aIAssistantService = aIAssistantService;
-            _parametricFunctions = parametricFunctions;
+            return BadRequest(
+                new ErrorResponseDTO()
+                {
+                    TextErrorTitle = "AtLeastOneNullParameter",
+                    TextErrorMessage = "Some parameters are null/missing.",
+                    TextErrorTrace = _parametricFunctions.GetCallerTrace()
+                }
+            );
         }
-
-        [HttpPost("/message")]
-        [ProducesResponseType(typeof(AIAssistantControllerPostMessageResponseDTO), 200)]
-        [ProducesResponseType(typeof(ErrorResponseDTO), 400)]
-        public async Task<ActionResult> PostMessage([FromBody] AIAssistantControllerPostMessageRequestDTO request)
-        {
-            if (!_parametricFunctions.ObjectExistsAndHasNoNullPublicProperties(request))
-            {
-                return BadRequest(
-                    new ErrorResponseDTO()
-                    {
-                        TextErrorTitle = "AtLeastOneNullParameter",
-                        TextErrorMessage = "Some parameters are null/missing.",
-                        TextErrorTrace = _parametricFunctions.GetCallerTrace()
-                    }
-                );
-            }
 
 #pragma warning disable CS8604
-            string textMessageResponse = await _aIAssistantService.SendMessageAndGetResponseAsync(request.TextMessage);
+        string textMessageResponse = await _aIAssistantService.SendMessageAndGetResponseAsync(request.TextMessage);
 #pragma warning restore CS8604
 
-            AIAssistantControllerPostMessageResponseDTO response = new()
-            {
-                TextMessage = textMessageResponse
-            };
+        AIAssistantControllerPostMessageResponseDTO response = new()
+        {
+            TextMessage = textMessageResponse
+        };
 
-            return Ok(response);
+        string? ioTHubConnectionString = _secretsService?.IoTHubSecrets?.ConnectionString;
+        if (ioTHubConnectionString != null)
+        {
+            var serviceClientForIoTHub = ServiceClient.CreateFromConnectionString(ioTHubConnectionString);
+            var seralizedMessage = JsonConvert.SerializeObject(textMessageResponse);
+
+            var ioTMessage = new Message(Encoding.UTF8.GetBytes(seralizedMessage));
+            await serviceClientForIoTHub.SendAsync(_appConfigurationsService.IoTDeviceName, ioTMessage);
         }
+
+        return Ok(response);
     }
 }
