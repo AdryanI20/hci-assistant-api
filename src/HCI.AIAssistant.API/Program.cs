d124a77a843cd5da360e9043d09ef48657f50861
AdryanI20 <gameder14@gmail.com>
2025-12-05 08:32:58 +0200

    fix

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -45,8 +45,8 @@ builder.Services.AddSingleton<IAppConfigurationsService>(
 provider => provider.GetRequiredService<IOptions<AppConfigurationsService>>().Value
 );
 builder.Services.AddSingleton<IParametricFunctions, ParametricFunctions>();
+builder.Services.AddSingleton<IAIAssistantService, AIAssistantService>();
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
